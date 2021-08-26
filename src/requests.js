114031bb5ddb716d610a8cf4af9912acf8ae55b5
AbefullStackDeveloper <84894159+AbefullStackDeveloper@users.noreply.github.com>
2021-08-26 02:17:29 -0500

    Netfilx -clone

---
+++ src/requests.js
@@ -0,0 +1,15 @@
+// https://www.themoviedb.org/talk/5daf6eb0ae36680011d7e6ee =link for change movie
+
+
+const API_KEY = "29046b463aff139a6caf675b3ead45d4";
+const requests = {
+  fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
+  fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
+  fetchTopRatedMovies: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
+  fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
+  fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
+  fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
+  fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
+  fetchDocumentaries: `/discover/movie?api_key=${API_KEY}&with_genres=99`,
+};
+export default requests;
